18775d84846e610777e528a16df6a157fd68033e
Anas khan <anas23khan083@gmail.com>
2024-12-04 01:17:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Navbar from "./components/Layout/Navbar";
 import Footer from "./components/Layout/Footer";
 import HomePage from "./components/HomePage/HomePage";
@@ -7,22 +7,31 @@ import Docs from "./components/Docs/Docs";
 import VideoStreaming from "./components/StreamingPage/VideoStreaming";
 import AdminPanel from "./components/AdminPanel/AdminPanel";
 import { UserProvider } from "./context/UserContext";
-import { SignedIn, SignedOut, RedirectToSignIn, useUser } from "@clerk/clerk-react";
+import { SignIn, useUser } from "@clerk/clerk-react";
 import MediaEditor from "./components/MediaEditor/MediaEditor";
 
+function PrivateRoute({ children }) {
+  const { isSignedIn } = useUser();
+
+  if (!isSignedIn) {
+    return <Navigate to="/sign-in" replace />;
+  }
+
+  return children;
+}
+
+function AdminRoute() {
+  const { user } = useUser();
+  const isAdmin = user?.emailAddresses[0]?.emailAddress.endsWith("@iiitdwd.ac.in");
+
+  if (!isAdmin) {
+    return <p>Unauthorized Access</p>;
+  }
+
+  return <AdminPanel />;
+}
+
 function App() {
-  
-  const AdminRoute = () => {
-    const { user } = useUser();
-    const isAdmin = user?.emailAddresses[0]?.emailAddress.endsWith("@iiitdwd.ac.in");
-  
-    if (!isAdmin) {
-      return <p>Unauthorized Access</p>;
-    }
-  
-    return <AdminPanel />;
-  };
-  
   return (
     <UserProvider>
       <Router>
@@ -35,44 +44,37 @@ function App() {
               <Route
                 path="/docs"
                 element={
-                  <SignedIn>
+                  <PrivateRoute>
                     <Docs />
-                  </SignedIn>
+                  </PrivateRoute>
                 }
               />
               <Route
                 path="/video-streaming"
                 element={
-                  <SignedIn>
+                  <PrivateRoute>
                     <VideoStreaming />
-                  </SignedIn>
+                  </PrivateRoute>
                 }
               />
               <Route
                 path="/admin"
                 element={
-                  <SignedIn>
+                  <PrivateRoute>
                     <AdminRoute />
-                  </SignedIn>
-                }
-              />
-               <Route
-                path="/media"
-                element={
-                  <SignedIn>
-                    <MediaEditor />
-                  </SignedIn>
+                  </PrivateRoute>
                 }
               />
               <Route
-                path="/sign-in"
+                path="/media"
                 element={
-                  <SignedOut>
-                    <RedirectToSignIn />
-                  </SignedOut>
+                  <PrivateRoute>
+                    <MediaEditor />
+                  </PrivateRoute>
                 }
+
               />
-              
+              <Route path="/sign-in" element={<SignIn />} />
             </Routes>
           </main>
 
